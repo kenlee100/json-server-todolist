dd1ec0e9c818a303a4b157ee8b25d4ee81a1e53e
kenlee <larukuhyde05@gmail.com>
2022-12-14 23:11:08 +0800

    feat 更新

--- web/src/js/all.js
+++ web/src/js/all.js
@@ -1,54 +1,54 @@
-const apiPath = 'http://localhost:3000';
-const input = document.querySelector('.input input');
-const btn_add = document.querySelector('.btn_add');
-const list = document.querySelector('.list');
-const list_footer = document.querySelector('.list_footer');
-const tab = document.querySelector('.tab');
-let currentTab = '全部';
+const apiPath = "http://localhost:3000";
+const input = document.querySelector(".input input");
+const btn_add = document.querySelector(".btn_add");
+const list = document.querySelector(".list");
+const list_footer = document.querySelector(".list_footer");
+const tab = document.querySelector(".tab");
+let currentTab = "全部";
 let todoDataList = [];
 let stateData = [];
-btn_add.addEventListener('click', (e) => {
+btn_add.addEventListener("click", (e) => {
   e.preventDefault();
   enterItem();
 });
 function enterItem() {
   const val = input.value;
-  input.value = '';
+  input.value = "";
   addItem(val);
 }
-input.addEventListener('keydown', (e) => {
+input.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     enterItem();
   }
 });
 
-list.addEventListener('click', (e) => {
+list.addEventListener("click", (e) => {
   // 刪除項目
-  if (e.target.classList.contains('delete')) {
+  if (e.target.classList.contains("delete")) {
     e.preventDefault();
     removeItem(e.target.dataset.id);
   }
 
   // 改變狀態
-  if (e.target.nodeName === 'INPUT') {
+  if (e.target.nodeName === "INPUT") {
     updateItem(e.target.id, e.target.checked);
   }
 });
-tab.addEventListener('click', (e) => {
+tab.addEventListener("click", (e) => {
   const tabText = e.target.textContent;
-  let stateText = '待完成';
+  let stateText = "待完成";
   switch (tabText) {
-    case '待完成':
-      currentTab = '待完成';
+    case "待完成":
+      currentTab = "待完成";
       stateData = filterData();
       break;
-    case '已完成':
-      stateText = '已完成';
-      currentTab = '已完成';
+    case "已完成":
+      stateText = "已完成";
+      currentTab = "已完成";
       stateData = filterData();
       break;
     default:
-      currentTab = '全部';
+      currentTab = "全部";
       stateData = filterData();
       break;
   }
@@ -60,23 +60,23 @@ tab.addEventListener('click', (e) => {
 // tab 狀態
 const tabListData = [
   {
-    name: '全部',
+    name: "全部",
     isActive: true,
   },
   {
-    name: '待完成',
+    name: "待完成",
     isActive: false,
   },
   {
-    name: '已完成',
+    name: "已完成",
     isActive: false,
   },
 ];
 function renderTabItem(currentTab) {
-  let str = '';
+  let str = "";
   tabListData.forEach((item) => {
     str += `
-    <li class="${currentTab === item.name ? 'active' : null}">${item.name}</li>
+    <li class="${currentTab === item.name ? "active" : null}">${item.name}</li>
     `;
   });
   tab.innerHTML = str;
@@ -99,27 +99,27 @@ function getDataList() {
 // 篩選完成/已完成狀態
 function filterData(data) {
   return todoDataList.filter((item) => {
-    if (currentTab === '已完成') {
+    if (currentTab === "已完成") {
       return item.isCompleted === true;
-    } else if (currentTab === '待完成') {
+    } else if (currentTab === "待完成") {
       return item.isCompleted === false;
-    } else if (currentTab == '全部') {
+    } else if (currentTab == "全部") {
       return item;
     }
   });
 }
-function renderFooter(data, state = '待完成') {
+function renderFooter(data, state = "待完成") {
   let str = `
     <p>${data} 個${state}項目</p>
     <a href="#" class="clear-btn">清除已完成項目</a>
   `;
   list_footer.innerHTML = str;
-  const clearBtn = document.querySelector('.clear-btn');
+  const clearBtn = document.querySelector(".clear-btn");
 
   // 清除已完成項目
-  clearBtn.addEventListener('click', (e) => {
+  clearBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    const comfirmDialog = confirm('確認全部清除?');
+    const comfirmDialog = confirm("確認全部清除?");
     if (comfirmDialog) {
       filterData().forEach((item) => {
         removeItem(item.id);
@@ -129,14 +129,16 @@ function renderFooter(data, state = '待完成') {
 }
 // 顯示todo
 function renderTodo(data) {
-  console.log('renderTodo', data.length);
-  let str = '';
+  let str = "";
+  if (data.length === 0) {
+    str += `<li class="d-flex justify-content-center"><span>沒有待辦事項喔!</span></li>`;
+  }
   data.forEach((item) => {
     str += `
      <li>
       <label class="checkbox" for="${item.id}" data-id="${item.id}">
         <input type="checkbox" id="${item.id}" ${
-      item.isCompleted ? 'checked' : ''
+      item.isCompleted ? "checked" : ""
     }/>
         <span>${item.content}</span>
       </label>
@@ -144,6 +146,7 @@ function renderTodo(data) {
     </li>
     `;
   });
+
   list.innerHTML = str;
 }
 function addItem(content) {
@@ -161,8 +164,6 @@ function addItem(content) {
     });
 }
 function updateItem(id, state) {
-  console.log(id, state);
-
   axios
     .patch(`${apiPath}/todos/${id}`, {
       isCompleted: state,
